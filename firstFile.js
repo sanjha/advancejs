d702fb06ce086a07327ebe5c7486264a5aa0e0dc
santosh kumar jha <santosh@nethuesindia.com>
2025-03-25 22:00:18 +0530

    Update firstFile.js

--- firstFile.js
+++ firstFile.js
@@ -1,1 +1,11 @@
-console.log("smith")
+const accountId = 12345
+let accountEmail = "santosh@test.com"
+var accountPassword = "testpassword"
+accountCity = "New Delhi"
+
+// accountId = 2 // not allowed
+accountEmail = "sk@sk.com"
+accountPassword = "newpassword"
+accountCity = "Jaipur"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity]);
