9e50154742624a5b45c17c67dbd49914b0626b6c
santosh kumar jha <santosh@nethuesindia.com>
2025-03-25 22:06:51 +0530

    Update firstFile.js

--- firstFile.js
+++ firstFile.js
@@ -2,10 +2,11 @@ const accountId = 12345
 let accountEmail = "santosh@test.com"
 var accountPassword = "testpassword"
 accountCity = "New Delhi"
+let accountState
 
 // accountId = 2 // not allowed
 accountEmail = "sk@sk.com"
 accountPassword = "newpassword"
 accountCity = "Jaipur"
 console.log(accountId)
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState]);
