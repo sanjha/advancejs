b5bfaa464f0ea000e49ee3129d1e4e0ad4aa9fd1
santosh kumar jha <santosh@nethuesindia.com>
2025-03-26 02:10:25 +0530

    gitignore

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
+let accountEmail = "santosh@test.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "New Delhi"
 let accountState;
 
 // accountId = 2 // not allowed
